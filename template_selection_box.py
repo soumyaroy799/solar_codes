8f5576dc1c64727b417f09e2336fe4b8f31a7f25
soumyaroy799 <40972701+soumyaroy799@users.noreply.github.com>
2025-06-13 14:29:51 +0530

    Code to selecte template for alignment

--- template_selection_box.py
+++ template_selection_box.py
@@ -1,13 +1,10 @@
-
 from matplotlib.widgets import RectangleSelector
 import matplotlib.pyplot as plt
 import astropy.units as u
-# author_gowria
 
 def select_roi_with_mouse(sunpy_map):
-    # Create a figure and explicitly add a WCSAxes subplot
     fig = plt.figure()
-    ax = fig.add_subplot(111, projection=sunpy_map) # Create ax first
+    ax = fig.add_subplot(111, projection=sunpy_map)
     ax.set_title("Select ROI (click and drag) then close the window")
     sunpy_map.plot(axes=ax)
     coords = []
@@ -15,30 +12,18 @@ def select_roi_with_mouse(sunpy_map):
     def onselect(eclick, erelease):
         coords.append((eclick.xdata, eclick.ydata, erelease.xdata, erelease.ydata))
 
-    toggle_selector = RectangleSelector(ax, onselect, useblit=True,
-                                        button=[1], minspanx=5, minspany=5, spancoords='pixels',
-                                        interactive=True)
+    RectangleSelector(ax, onselect, useblit=True,
+                      button=[1], minspanx=5, minspany=5, spancoords='pixels',
+                      interactive=True)
     plt.show()
 
     if not coords:
         raise RuntimeError("ROI selection cancelled or failed.")
 
     x1, y1, x2, y2 = coords[0]
-
     bottom_left = (min(x1, x2), min(y1, y2)) * u.pixel
     top_right = (max(x1, x2), max(y1, y2)) * u.pixel
 
     submap = sunpy_map.submap(bottom_left=bottom_left, top_right=top_right)
     return submap
 
-if __name__=="__main__" :
-    """
-    # Loading the files and sorting an dtaking the first image as reference
-    """
-    ref_img = sunpy.map.Map(sorted_files[ref_idx])
-    logger.info(f"Using reference image: {sorted_files[ref_idx]}")
-    ref_submap = select_roi_with_mouse(ref_img)
-
-    """
-    Rest of the code
-    """
